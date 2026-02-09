dbd93ec0b38e04d53eab1b646c26b39f5bac384c
nwiedel <164350832+nwiedel@users.noreply.github.com>
2026-02-09 23:10:33 +0100

    3.3.1. Animations final

--- core/src/main/java/de/nicolas/Chapter33Animation.java
+++ core/src/main/java/de/nicolas/Chapter33Animation.java
@@ -33,7 +33,7 @@ public class Chapter33Animation extends ApplicationAdapter {
     public void create() {
         batch = new SpriteBatch();
         assetManager = new AssetManager();
-        assetManager.load("test.atlas", TextureAtlas.class);
+        assetManager.load("p1_walk/test.atlas", TextureAtlas.class);
     }
 
     @Override
@@ -42,27 +42,27 @@ public class Chapter33Animation extends ApplicationAdapter {
 
         if (assetManager.update()){
             if (atlas == null) {
-                atlas = assetManager.get("test.atlas", TextureAtlas.class);
+                atlas = assetManager.get("p1_walk/test.atlas", TextureAtlas.class);
 
                 // get all regions for the walking animation
                 Array<TextureRegion> walkingFrames = new Array<>();
-                walkingFrames.add(atlas.findRegion("p1_walk01"));
-                walkingFrames.add(atlas.findRegion("p1_walk02"));
-                walkingFrames.add(atlas.findRegion("p1_walk03"));
-                walkingFrames.add(atlas.findRegion("p1_walk04"));
-                walkingFrames.add(atlas.findRegion("p1_walk05"));
-                walkingFrames.add(atlas.findRegion("p1_walk06"));
-                walkingFrames.add(atlas.findRegion("p1_walk07"));
-                walkingFrames.add(atlas.findRegion("p1_walk08"));
-                walkingFrames.add(atlas.findRegion("p1_walk09"));
-                walkingFrames.add(atlas.findRegion("p1_walk101"));
-                walkingFrames.add(atlas.findRegion("p1_walk11"));
+                walkingFrames.add(atlas.findRegion("p1_walk", 1));
+                walkingFrames.add(atlas.findRegion("p1_walk", 2));
+                walkingFrames.add(atlas.findRegion("p1_walk", 3));
+                walkingFrames.add(atlas.findRegion("p1_walk", 4));
+                walkingFrames.add(atlas.findRegion("p1_walk", 5));
+                walkingFrames.add(atlas.findRegion("p1_walk", 6));
+                walkingFrames.add(atlas.findRegion("p1_walk", 7));
+                walkingFrames.add(atlas.findRegion("p1_walk", 8));
+                walkingFrames.add(atlas.findRegion("p1_walk", 9));
+                walkingFrames.add(atlas.findRegion("p1_walk", 10));
+                walkingFrames.add(atlas.findRegion("p1_walk", 11));
                 walkingAnimation = new Animation<>(0.08f, walkingFrames);
 
                 // get all regions for the idle animation
-                Array<TextureRegion> idleframes = new Array<>();
-                idleframes.add(atlas.findRegion("p1_walk02"));
-                idleAnimation = new Animation<>(0.12f, idleframes);
+                Array<TextureRegion> idleFrames = new Array<>();
+                idleFrames.add(atlas.findRegion("p1_walk", 2));
+                idleAnimation = new Animation<>(0.12f, idleFrames);
 
                 currentState = State.IDLE;
             }
@@ -80,7 +80,33 @@ public class Chapter33Animation extends ApplicationAdapter {
                 currentState = State.IDLE;
             }
 
-            
+            // get the correct frame from the correct animation based on the current state
+            if (currentState == State.WALKING){
+                currentFrame = walkingAnimation.getKeyFrame(stateTime,true);
+            } else {
+                currentFrame = idleAnimation.getKeyFrame(stateTime, true);
+            }
+
+            batch.begin();
+            batch.draw(currentFrame, (float)(Gdx.graphics.getWidth() /2 - currentFrame.getRegionWidth() /2),
+                (float)(Gdx.graphics.getHeight() /2 - currentFrame.getRegionHeight() /2),
+                currentFrame.getRegionWidth() * 2, currentFrame.getRegionHeight() * 2);
+            batch.end();
+        }
+
+        else {
+            // Display loading progress
+            Gdx.app.log("Loading", "Progress: " + assetManager.getProgress() * 100 + "%");
+        }
+    }
+
+    @Override
+    public void dispose() {
+        if (batch != null){
+            batch.dispose();
+        }
+        if (assetManager != null){
+            assetManager.dispose();
         }
     }
 }
--- lwjgl3/src/main/java/de/nicolas/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/de/nicolas/lwjgl3/Lwjgl3Launcher.java
@@ -12,7 +12,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Chapter32TexturePacker(), getDefaultConfiguration());
+        return new Lwjgl3Application(new Chapter33Animation(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
