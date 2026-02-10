8c9201ff24a70e1a1e0332804642c7978ddde872
nicolaswiedel <nicolaswiedel@googlemail.com>
2026-02-10 17:43:15 +0100

    4.1. Input pooling

---
+++ core/src/main/java/de/nicolas/Chapter41Input.java
@@ -0,0 +1,81 @@
+package de.nicolas;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Chapter41Input extends ApplicationAdapter {
+
+    private SpriteBatch batch;
+    private Sprite playerSprite;
+    private float playerSpeed = 200.0f;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        Texture playerTexture = new Texture("p1_walk/p1_walk01.png");
+        playerSprite = new Sprite(playerTexture);
+        playerSprite.setPosition(Gdx.graphics.getWidth() /2f - playerSprite.getWidth() / 2f,
+            Gdx.graphics.getHeight() / 2f - playerSprite.getHeight() / 2f);
+    }
+
+    @Override
+    public void render() {
+        // get the time since the last frame for frame rate independent movement
+        float deltaTime = Gdx.graphics.getDeltaTime();
+
+        // check keyboard input
+        if (Gdx.input.isKeyPressed(Input.Keys.A)){
+            playerSprite.translateX(playerSpeed * deltaTime);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)){
+            playerSprite.translateX(-playerSpeed * deltaTime);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)){
+            playerSprite.translateY(playerSpeed * deltaTime);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)){
+            playerSprite.translateY(-playerSpeed * deltaTime);
+        }
+
+        // Check for touch input (mobile/desktop)
+        if (Gdx.input.isTouched()){
+            int touchX = Gdx.input.getX();
+            int touchY = Gdx.input.getY();
+
+            // move the sprite towards the touchpoint
+            if (touchX > playerSprite.getX() + playerSprite.getWidth() / 2f){
+                playerSprite.translateX(playerSpeed * deltaTime);
+                Gdx.app.log("touchX: " + touchX, "playerSpriteX: " + playerSprite.getX() + playerSprite.getWidth() / 2f);
+            }
+            if (touchX < playerSprite.getX() + playerSprite.getWidth() / 2f) {
+                playerSprite.translateX(-playerSpeed * deltaTime);
+                Gdx.app.log("touchX: " + touchX, "playerSpriteX: " + playerSprite.getX() + playerSprite.getWidth() / 2f);
+            }
+            if (touchY > playerSprite.getY() + playerSprite.getHeight() / 2f){
+                playerSprite.translateY(-playerSpeed * deltaTime);
+                Gdx.app.log("touchY: " + touchY, "playerSpriteY: " + playerSprite.getY() + playerSprite.getHeight() / 2f);
+            }
+            if (touchY < playerSprite.getY() + playerSprite.getHeight() / 2f){
+                playerSprite.translateY(playerSpeed * deltaTime);
+                Gdx.app.log("touchY: " + touchY, "playerSpriteY: " + playerSprite.getY() + playerSprite.getHeight() / 2f);
+            }
+        }
+
+        ScreenUtils.clear(0.2f, 0.2f, 0.2f,1f);
+
+        batch.begin();
+        playerSprite.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        playerSprite.getTexture().dispose();
+    }
+}
--- lwjgl3/src/main/java/de/nicolas/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/de/nicolas/lwjgl3/Lwjgl3Launcher.java
@@ -12,7 +12,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Chapter33Animation(), getDefaultConfiguration());
+        return new Lwjgl3Application(new Chapter41Input(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
